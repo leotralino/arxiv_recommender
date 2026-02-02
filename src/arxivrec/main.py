ec21c7662120485132f168cc40e732619669f4f7
Leotrinos <yanglyu2@berkeley.edu>
2026-02-02 01:34:25 -0800

    config with yaml

--- src/arxivrec/main.py
+++ src/arxivrec/main.py
@@ -1,5 +1,8 @@
+import argparse
 import logging
 import sys
+
+import yaml
 
 from arxivrec.encoder import TextEncoder
 from arxivrec.fetcher import ArxivFetcher
@@ -13,22 +16,37 @@ setup_logging()
 logger = logging.getLogger(__name__)
 
 
+def load_config(config_path: str = "config.yaml"):
+    with open(config_path, "r") as f:
+        return yaml.safe_load(f)
+
+
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, default="src/config.yaml")
+    args = parser.parse_args()
+
+    cfg = load_config(args.config)
+
     curr_topic = Topic(
-        id="AI",
-        description=(
-            "Retrieval augmented generation (RAG)," "document parsing, table extraction"
-        ),
-        categories=["cs.AI"],
+        id=cfg["topic"]["id"],
+        description=cfg["topic"]["description"],
+        categories=cfg["topic"]["categories"],
     )
-    fetcher = ArxivFetcher(topic=curr_topic, lookback_days=4, max_results=100)
-    encoder = TextEncoder(model_name="sentence-transformers/all-MiniLM-L6-v2")
-    ranker = OLLMRanker(model_name="llama3.2:3b")
+
+    fetcher = ArxivFetcher(
+        topic=curr_topic,
+        lookback_days=cfg["pipeline"]["lookback_days"],
+        max_results=cfg["pipeline"]["max_results"],
+    )
+
+    encoder = TextEncoder(model_name=cfg["models"]["encoder"])
+    ranker = OLLMRanker(model_name=cfg["models"]["ranker"])
     notifier = EmailNotifier()
 
     pipeline = OLLMPipeline(
         topic=curr_topic,
-        simsearch_top_k=10,
+        simsearch_top_k=cfg["pipeline"]["simsearch_top_k"],
         fetcher=fetcher,
         encoder=encoder,
         ollm_ranker=ranker,
@@ -44,7 +62,8 @@ def main():
         sys.exit(1)
 
     try:
-        pipeline.notify()
+        if cfg["notifiers"]["email"]:
+            pipeline.notify()
     except Exception as e:
         logger.exception(f"Error when sending notificatio: {e}")
         sys.exit(1)
